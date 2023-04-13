55127410a806ab7eb67aa171d8040147fd1a85d3
Almkhlafi <azaymabood2015@gmail.com>
2023-04-13 04:20:49 +0300

    LiteNotepad Unfinished Project

---
+++ NotePadLite/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace NotePadLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolbSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.btnBold = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnItalic = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnUnderline = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnNewFile = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnClose = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 0, 2);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(776, 397);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolbSearch,
+            this.btnBold,
+            this.toolStripSeparator1,
+            this.btnItalic,
+            this.toolStripSeparator2,
+            this.btnUnderline,
+            this.toolStripSeparator3,
+            this.btnNewFile,
+            this.toolStripSeparator4,
+            this.btnClose});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 38);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(40, 32);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // toolbSearch
+            // 
+            this.toolbSearch.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolbSearch.Margin = new System.Windows.Forms.Padding(1, 0, 2, 0);
+            this.toolbSearch.Name = "toolbSearch";
+            this.toolbSearch.Size = new System.Drawing.Size(100, 38);
+            // 
+            // btnBold
+            // 
+            this.btnBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnBold.Image = ((System.Drawing.Image)(resources.GetObject("btnBold.Image")));
+            this.btnBold.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnBold.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(40, 32);
+            this.btnBold.Text = "toolStripButton2";
+            this.btnBold.ToolTipText = "Bold";
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnItalic.Image = ((System.Drawing.Image)(resources.GetObject("btnItalic.Image")));
+            this.btnItalic.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnItalic.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(40, 32);
+            this.btnItalic.Text = "toolStripButton3";
+            this.btnItalic.ToolTipText = "Italic";
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 38);
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnUnderline.Image = ((System.Drawing.Image)(resources.GetObject("btnUnderline.Image")));
+            this.btnUnderline.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnUnderline.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(40, 32);
+            this.btnUnderline.Text = "toolStripButton4";
+            this.btnUnderline.ToolTipText = "Under Line";
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 38);
+            // 
+            // btnNewFile
+            // 
+            this.btnNewFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNewFile.Image = ((System.Drawing.Image)(resources.GetObject("btnNewFile.Image")));
+            this.btnNewFile.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnNewFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNewFile.Name = "btnNewFile";
+            this.btnNewFile.Size = new System.Drawing.Size(40, 32);
+            this.btnNewFile.Text = "toolStripButton5";
+            this.btnNewFile.ToolTipText = "New Notepad";
+            this.btnNewFile.Click += new System.EventHandler(this.btnNewFile_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 38);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(40, 32);
+            this.btnClose.Text = "toolStripButton2";
+            this.btnClose.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripTextBox toolbSearch;
+        private System.Windows.Forms.ToolStripButton btnBold;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnItalic;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnUnderline;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton btnNewFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton btnClose;
+    }
+}
+
---
+++ NotePadLite/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePadLite
+{
+    public partial class Form1 : Form
+    {
+
+        bool isBold = false;
+        bool isIralic = false;
+        bool isUnderline = false;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewFile_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+        }
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            if (isUnderline) {
+
+                this.isUnderline = false;
+                toNormaize();
+            } else {
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Underline
+                  | richTextBox.SelectionFont.Style);
+                this.isUnderline = true;
+            }
+            
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            if (isIralic)
+            {
+
+                this.isIralic = false;
+                toNormaize();
+            }
+            else
+            {
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Italic
+                  | richTextBox.SelectionFont.Style);
+                this.isIralic = true;
+            }
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            if (isBold)
+            {
+
+                this.isBold = false;
+                toNormaize();
+            }
+            else
+            {
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Bold
+                  | richTextBox.SelectionFont.Style);
+                this.isBold = true;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toNormaize()
+        {
+        
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Regular);
+            if(isBold)
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Bold);
+            if (isIralic)
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Italic);
+            if (isUnderline)
+                this.richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Underline);
+        }
+
+    }
+}
